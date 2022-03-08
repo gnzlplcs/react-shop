ada7f1e50c3799323c7a5508fe3c79b0abcefe3e
gnzlplcs <palaciosgm@outlook.com>
2022-03-08 14:46:45 -0500

    useEffect and API consumption

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ProductItem } from '@components/ProductItem';
 import '@styles/ProductList.scss';
+import axios from 'axios';
+
+const API = 'https://api.escuelajs.co/api/v1/products'
 
 export const ProductList = () => {
+	const [products, setProduts] = useState([]);
+
+	useEffect(async () => {
+		const response = await axios(API);
+		setProduts(response.data)
+	}, []);
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(product => (
+					<ProductItem />
+				))}
 			</div>
 		</section>
 	);
