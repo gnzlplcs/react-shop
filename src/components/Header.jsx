344e975a3d5f5a73f90dab1f743ba3f9d3de5829
gnzlplcs <palaciosgm@outlook.com>
2022-03-16 22:02:58 -0500

    :sparkles: edit purchase order

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from "react";
 import { Menu } from "@components/Menu";
+import { MyOrder } from "@containers/MyOrder";
 import { AppContext } from "../context/AppContext";
 import "@styles/Header.scss";
 
@@ -9,6 +10,7 @@ import shoppingCart from "@icons/icon_shopping_cart.svg";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
+  const [toggleOrders, setToggleOrders] = useState(false);
   const { state } = useContext(AppContext);
 
   const handleToggle = () => {
@@ -46,13 +48,17 @@ const Header = () => {
           <li className="navbar-email" onClick={handleToggle}>
             platzi@example.com
           </li>
-          <li className="navbar-shopping-cart">
+          <li
+            className="navbar-shopping-cart"
+            onClick={() => setToggleOrders(!toggleOrders)}
+          >
             <img src={shoppingCart} alt="shopping cart" />
             {state.cart.length > 0 && <div>{state.cart.length}</div>}
           </li>
         </ul>
       </div>
       {toggle && <Menu />}
+      {toggleOrders && <MyOrder />}
     </nav>
   );
 };
