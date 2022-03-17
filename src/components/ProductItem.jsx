ce01d2a31594565c4dd727c7a299c69eca27bb0b
gnzlplcs <palaciosgm@outlook.com>
2022-03-16 21:12:43 -0500

    :construction: modify ProductItem component
    add useContext

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 import "@styles/ProductItem.scss";
 
-import addToCart from "@icons/bt_add_to_cart.svg";
+import addToCartImg from "@icons/bt_add_to_cart.svg";
 
 const ProductItem = ({ product }) => {
+  const { addToCart } = useContext(AppContext)
+
+  const handleClick = (item) => {
+    addToCart(item);
+  };
   return (
     <div className="ProductItem">
       <img src={product.images[0]} alt={product.title} />
@@ -12,8 +18,8 @@ const ProductItem = ({ product }) => {
           <p>${product.price}</p>
           <p>{product.title}</p>
         </div>
-        <figure>
-          <img src={addToCart} alt="" />
+        <figure onClick={() => handleClick(product)}>
+          <img src={addToCartImg} alt="" />
         </figure>
       </div>
     </div>
